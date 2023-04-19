d1b789db8035e4a2d1f90e74a58c0736bd170608
MarkGuo2002 <100450691@alumnos.uc3m.es>
2023-04-19 12:40:39 +0200

    hola

---
+++ kai-index.js
@@ -0,0 +1,91 @@
+const path = require('path');
+const http = require('http');
+const socketIO = require('socket.io');
+const express = require('express');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const PORT = 3000; // Port to listen on
+
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+
+app.use(express.static(path.join(__dirname))); // Serve static files
+
+
+app.get('/phone', (req, res) => {
+    res.sendFile(path.join(__dirname, 'phone/phone-index.html'));
+});
+
+app.get("phone/phone-script.js", (req, res) => {
+    res.sendFile(path.join(__dirname, 'phone/phone-script.js'));
+});
+
+// app.get("phone/phone-styles.css", (req, res) => {
+//     res.sendFile(path.join(__dirname, 'phone/phone-styles.css'));
+// });
+
+app.get("/fav", (req, res) => {
+    res.sendFile(path.join(__dirname, 'phone/phone-fav.html'));
+});
+
+app.get("phone/phone-fav.css", (req, res) => {
+    res.sendFile(path.join(__dirname, 'phone/phone-fav.css'));
+});
+
+app.get("tv/tv-index.html", (req, res) => {
+    res.sendFile(path.join(__dirname, 'tv/tv-index.html'));
+});
+
+app.get("tv/tv-script.js", (req, res) => {
+    res.sendFile(path.join(__dirname, 'tv/tv-script.js'));
+});
+
+app.get("tv/tv-styles.css", (req, res) => {
+    res.sendFile(path.join(__dirname, 'tv/tv-styles.css'));
+});
+
+app.get("socket.io/socket.io.js", (req, res) => {
+    res.sendFile(path.join(__dirname, 'node_modules', 'socket.io', 'client-dist', 'socket.io.min.js'));
+});
+
+
+//if the url does not exist, report 404
+app.get('*', (req, res) => {
+    res.status(404).send('404 - Page not found');
+});
+
+
+
+
+
+io.on('connection', (socket) => { // Listen for connection
+    console.log('A user connected'); // Log connection
+
+    socket.on('icon-clicked', (data) => { // Listen for icon click
+        console.log(`Icon clicked: ${data.iconId}`);
+        const message = {
+            type: 'iconClicked',
+            iconId: data.iconId
+        };
+        sendMessage(message); // Send message to tv
+    });
+
+    socket.on('disconnect', () => { // Listen for disconnection
+        console.log('User disconnected');
+    });
+});
+
+server.listen(PORT, () => { // Listen on port
+    console.log(`Server listening on port ${PORT}`);
+});
+
+function sendMessage(message) { // Send message to tv
+    io.emit('message', message);
+    console.log("Message sent");
+}
--- tv/tv-script.js
+++ tv/tv-script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3000'); // Connect to the server
+//const socket = io('http://localhost:3000'); // Connect to the server
 let user = document.getElementById("user");
 let userDropdown = document.querySelector(".user-dropdown");
 let videoContainer = document.querySelector(".video-container");
@@ -70,57 +70,57 @@ populateVideos();
 let i = 0;
 let selectedId = INTERFACES[i].id;
 console.log('selectedId', selectedId);
- 
-socket.on('connect', () => { // Listen for connection
-  console.log('Connected to server');
-});
 
-socket.on('message', (message) => { // Listen for message
-  console.log(`Message recived: ${message.iconId}`);
-  handleRequest(message.iconId); // Handle request
-});
+// socket.on('connect', () => { // Listen for connection
+//     console.log('Connected to server');
+// });
 
-socket.on('disconnect', () => { // Listen for disconnection
-  console.log('Disconnected from server');
-});
+// socket.on('message', (message) => { // Listen for message
+//     console.log(`Message recived: ${message.iconId}`);
+//     handleRequest(message.iconId); // Handle request
+// });
 
-   
-function handleRequest(iconId) { // Handle request
-    switch (iconId) {
-        case 'volume-up':
-            console.log('Volume up');
-            break;
-        case 'volume-down':
-            console.log('Volume down');
-            break;
-        case 'arrow-up':
-            console.log('Arrow up');
-            break;
-        case 'arrow-down':
-            console.log('Arrow down');
-            break;
-        case 'arrow-left':
-            console.log('Arrow left');
-            break;
-        case 'arrow-right':
-            console.log('Arrow right');
-            break;
-        case 'play-pause':
-            console.log('Play/pause');
-            break;
-        case 'gestures':
-            console.log('Gestures');
-            break;
-        case 'go-back':
-            console.log('Go back');
-            break;
-        case 'okay':
-            console.log('Okay');
-            break;  
-        default:
-            console.log('Unknown request');
-    }
-}
+// socket.on('disconnect', () => { // Listen for disconnection
+//     console.log('Disconnected from server');
+// });
+
+
+// function handleRequest(iconId) { // Handle request
+//     switch (iconId) {
+//         case 'volume-up':
+//             console.log('Volume up');
+//             break;
+//         case 'volume-down':
+//             console.log('Volume down');
+//             break;
+//         case 'arrow-up':
+//             console.log('Arrow up');
+//             break;
+//         case 'arrow-down':
+//             console.log('Arrow down');
+//             break;
+//         case 'arrow-left':
+//             console.log('Arrow left');
+//             break;
+//         case 'arrow-right':
+//             console.log('Arrow right');
+//             break;
+//         case 'play-pause':
+//             console.log('Play/pause');
+//             break;
+//         case 'gestures':
+//             console.log('Gestures');
+//             break;
+//         case 'go-back':
+//             console.log('Go back');
+//             break;
+//         case 'okay':
+//             console.log('Okay');
+//             break;
+//         default:
+//             console.log('Unknown request');
+//     }
+// }
 
 // Add selected class to initial item
 document.getElementById(selectedId).classList.add("selected");
@@ -232,6 +232,7 @@ document.addEventListener("keydown", (e) => {
 
 
 function populateVideos() {
+    alert("populate");
     console.log("populate");
     //skipping headers
 
@@ -249,7 +250,7 @@ function populateVideos() {
 
         videoCard.appendChild(video);
         videoCard.appendChild(title);
-        //videoContainer.appendChild(videoCard);
+        videoContainer.appendChild(videoCard);
 
 
     }
